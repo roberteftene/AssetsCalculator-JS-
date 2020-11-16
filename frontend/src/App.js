00e621373c6e072991117c608affa133ae4fd479
roberteftene <roberteftene25@gmail.com>
2020-11-16 18:59:49 +0200

    implemented todolist

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+//Importing Components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+function App() {
+  //State
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //Use effect
+  useEffect(() => {
+    filterHandler();
+  }, [todos,status]);
+
+  //Functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+
+       
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} />
+    </div>
+  );
+}
+
+export default App;
